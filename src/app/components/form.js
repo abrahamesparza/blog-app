ff7689be3e32b976e681d1b84c9b7c81fa2b03f7
Abraham Esparza <abrahamesparza.dev@gmail.com>
2024-08-28 19:57:18 -0700

    Send user form data to user api

--- src/app/components/form.js
+++ src/app/components/form.js
@@ -1,21 +1,67 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from '../page.module.css';
 
 export default function Form() {    
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password: '',
+    });
+
+    async function onChange(event) {
+        const { name, value } = event.target
+        setFormData(prevData => ({
+            ...prevData,
+            [name]: value
+        }))
+    }
+
     async function onSubmit(event) {
         event.preventDefault();
 
-        const formData = new FormData(event.target);
-        console.log('form data: ', formData);
+        let data = JSON.stringify(formData);
+        const response = await fetch('/api/user', {
+            method: 'POST',
+            body: data,
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+
+        const result = await response.json();
+        console.log(`Result: ${result.message}`);
     }
 
     return (
         <div className={styles.formBody}>
             <form onSubmit={onSubmit} className={styles.form}>
-                <input type="text" className={styles.input} name="mame"/>
-                <input type="text" className={styles.input} name="email"/>
-                <input type="password" className={styles.input} name="password"/>
-                <button type="submit" className={styles.formButton}>Submit</button>
+                <label>Name</label>
+                <input
+                onChange={onChange}
+                type="text"
+                className={styles.input}
+                name="name"
+                value={formData.name}
+                />
+                <label>Email</label>
+                <input
+                onChange={onChange}
+                type="email"
+                className={styles.input}
+                name="email"
+                value={formData.email}
+                />
+                <label>Password</label>
+                <input
+                onChange={onChange}
+                type="password"
+                className={styles.input}
+                name="password"
+                value={formData.password}
+                />
+                <button
+                type="submit"
+                className={styles.formButton}>Submit</button>
             </form>
         </div>
     )
