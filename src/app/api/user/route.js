ba4eddb00a8776e20b63e124f0d1909ac5b59ce6
Abraham Esparza <abrahamesparza.dev@gmail.com>
2024-08-29 20:38:59 -0700

    Store user data in users table

--- src/app/api/user/route.js
+++ src/app/api/user/route.js
@@ -1,8 +1,29 @@
-import NextRequest, { NextResponse } from 'next/server';
+import dynamoDB from '../lib/dynamodb';
+import { NextResponse } from 'next/server';
+
+const { v4: uuidv4 } = require('uuid');
 
 export async function POST(request) {
-    const data = await request.json();
-    const userData = JSON.stringify(data)
-    console.log(`User Data received: ${userData}`);
-    return NextResponse.json({'message': 'Data retrieved successfully.'});
+    try {
+        const data = await request.json();
+    
+        const uniqueId = uuidv4();
+        const params = {
+            TableName: 'users',
+            Item: {
+                id: uniqueId,
+                name: data.name,
+                email: data.email,
+                password: data.password,
+            },
+        };
+        
+        await dynamoDB.put(params).promise();
+        return NextResponse.json({'message': 'Data stored successfully.'});
+    }
+    catch(error) {
+        console.error(`Error storing data', ${error}`)
+        return NextResponse.error()
+    }
+
 }
