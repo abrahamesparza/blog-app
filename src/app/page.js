1076863960f78ebad25b5800fb1e0b4af744ece0
Abraham Esparza <abrahamesparza.dev@gmail.com>
2024-10-21 16:08:02 -0700

    Remove unused code

--- src/app/page.js
+++ src/app/page.js
@@ -1,38 +1,20 @@
 'use client'
 
 import React, { useState } from "react";
-import { MdOutlineMenu } from "react-icons/md";
-import { useRouter } from "next/navigation";
 
 import styles from "./page.module.css";
-import SignupForm from "./components/signup/signupForm";
-import LoginForm from "./components/login/loginForm";
+import SignupForm from "./signup/page";
+import LoginForm from "./login/page";
 import FormTypes from "./components/formTypes";
 
 
 export default function Home() {
-  const router = useRouter();
   const [form, setForm] = useState('');
 
   function handleForm(e) {
     let formType = e.target.innerText;
-    console.log('form type: ', formType)
     setForm(formType)
   }
-
-  function updateForm(formType) {
-    setForm(formType)
-  }
-
-  function handleSignup() {
-    console.log('in handle signup')
-    router.push('/signup');
-  };
-
-  function handleLogin() {
-    console.log('in handle login')
-    router.push('/login');
-  };
 
   return (
     <main>
@@ -41,8 +23,8 @@ export default function Home() {
         </div>
         <div>
           {
-            form === 'Sign Up' ? <SignupForm onClick={handleSignup} formType={updateForm}/>
-            : form === 'Log In' ? <LoginForm onClick={handleLogin} formType={updateForm}/>
+            form === 'Sign Up' ? <SignupForm />
+            : form === 'Log In' ? <LoginForm />
             : <FormTypes formType={handleForm}/>
           }
         </div>
