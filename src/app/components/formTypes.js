4d21f8aa440438b47f569ed7e83f77ed688ad675
Abraham Esparza <abrahamesparza.dev@gmail.com>
2024-10-21 16:08:19 -0700

    Handle routing

--- src/app/components/formTypes.js
+++ src/app/components/formTypes.js
@@ -1,12 +1,25 @@
 import React from "react";
 import styles from '../page.module.css';
+import { useRouter } from "next/navigation";
 
-export default function FormTypes({ formType }) {
+export default function FormTypes() {
+    const router = useRouter();
+    
+    function handleRouting(e) {
+        let page = e.target.innerText;
+        if (page === 'Sign Up') {
+            page = 'signup'
+        }
+        else if (page === 'Log In') {
+            page = 'login'
+        }
+        router.push(`/${page}`);
+      };
 
     return (
         <div className={styles.formOption}>
-            <h2 onClick={formType}>Sign Up</h2>
-            <h2 onClick={formType}>Log In</h2>
+            <h2 onClick={handleRouting}>Sign Up</h2>
+            <h2 onClick={handleRouting}>Log In</h2>
         </div>
     )
 }
