9e572c3fe7f87affe00860dcda479479204024a2
Abraham Esparza <abrahamesparza.dev@gmail.com>
2024-10-23 21:26:43 -0700

    Add data generation call to useEffect for testing

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,23 @@
 'use client'
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import styles from "./page.module.css";
 import SignupForm from "./signup/page";
 import LoginForm from "./login/page";
 import FormTypes from "./components/formTypes";
 
+import { generateUserData } from './data/dataGeneration';
 
 export default function Home() {
+
+  // use to generate data in console for testing
+  useEffect(() => {
+    const data = JSON.stringify(generateUserData());
+    
+    console.log(`user data: ${data}`);
+  }, [])
+
   const [form, setForm] = useState('');
 
   function handleForm(e) {
