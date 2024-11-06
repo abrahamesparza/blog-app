9c6edf7740635295ad76dea6a85a7da61cfe37bd
Abraham Esparza <abrahamesparza.dev@gmail.com>
2024-11-05 16:37:43 -0800

    correct spelling for placeholder data

--- src/app/landing/page.js
+++ src/app/landing/page.js
@@ -30,7 +30,7 @@ export default function HomePage() {
         },
         {
         'name': 'meowdas',
-        'title': 'the mewodiest'
+        'title': 'the meowdiest'
         },
         {
         'name': 'snoop',
