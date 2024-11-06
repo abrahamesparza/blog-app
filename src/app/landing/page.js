05e4eb5face2542238ad2c6e6791cd24e59899ac
Abraham Esparza <abrahamesparza.dev@gmail.com>
2024-11-05 16:34:04 -0800

    Add placeholder data to landing page

--- src/app/landing/page.js
+++ src/app/landing/page.js
@@ -15,11 +15,72 @@ export default function HomePage() {
     - Rewrite landing page to show generated blog data
     - Move the below code to a writeBlog component
     */
+   let fakeBlogs = [
+        {
+        'name': 'bob',
+        'title': 'the builder'
+        },
+        {
+        'name': 'cookie',
+        'title': 'the monster'
+        },
+        {
+        'name': 'oscar',
+        'title': 'the grouch'
+        },
+        {
+        'name': 'meowdas',
+        'title': 'the mewodiest'
+        },
+        {
+        'name': 'snoop',
+        'title': 'the d o double g'
+        },
+        {
+        'name': 'juice',
+        'title': 'the world'
+        },
+        {
+        'name': 'buzz',
+        'title': 'the lightyear'
+        },
+        {
+        'name': 'midas',
+        'title': 'the rex'
+        },
+        {
+        'name': 'rick',
+        'title': 'the morty'
+        },
+        {
+        'name': 'woody',
+        'title': 'the cowboy'
+        },
+];
     return (
         <div className={styles.landingContainer}>
-            <h1>Blog content goes here</h1>
-            <h2>Write a blog</h2>
-            <Logout reroute={'/'}/>
+            <div className={styles.navigation}>
+                {/* improvement idea:
+                    create a modal to display below items
+                    stacked one above the other
+                */}
+                <p>Write</p>
+                <p>Explore</p>
+                <p>Profile</p>
+                <Logout reroute={'/'}/>
+            </div>
+            <div className={styles.feedContainer}>
+                {fakeBlogs.map((item, index) => (
+                    <div>
+                        <p
+                        className={styles.blogItem}
+                        key={index}>
+                            {item.name}<br/>
+                            {item.title}
+                        </p>
+                    </div>
+                ))} 
+            </div>
         </div>
     )
 }
