645c4fbfe0063f2779d3e0490aa3ca7bafe7ab41
Abraham Esparza <abrahamesparza.dev@gmail.com>
2024-11-18 21:07:45 -0800

    Write batchWrite script to upload user data (WIP)

--- src/app/data/dataGeneration.js
+++ src/app/data/dataGeneration.js
@@ -1,5 +1,6 @@
 import { uniqueNamesGenerator, adjectives, animals, colors, names } from "unique-names-generator";
-const { v4: uuidv4 } = require('uuid');
+
+import { v4 } from "uuid";
 import { LoremIpsum } from "lorem-ipsum";
 
 import dynamoDB from '../api/lib/dynamodb.js';
@@ -35,57 +36,94 @@ const lorem = new LoremIpsum({
 });
 
 const generateUniqueId = () => {
-    const uniqueId = uuidv4();
+    const uniqueId = v4();
     return uniqueId
 }
 
-const generateBlogData = async () => {
+const generateBlogData = () => {
     const paragraphAmount = Math.floor((Math.random() * 5) + 1);
     const titleAmount = Math.floor((Math.random() * 10) + 1);
+    const blogCount = Math.floor(Math.random() * 9 + 1);
+    const userBlogs = [];
 
-    for (let i = 0; i < 1000; i++) {
-        const uniqueId = generateUniqueId();
+    for (let i = 0; i < Math.max(blogCount, 1); i++) {
         const blogTitle = lorem.generateWords(titleAmount);
         const blogContent = lorem.generateParagraphs(paragraphAmount);
         const blogData = {
-            id: uniqueId,
+            id: i + 1,
             title: blogTitle,
             content: blogContent,
         };
-        
-        try {
-            await dynamoDB.put({
-                TableName: 'blog-data',
-                Item: blogData,
-            }).promise();
-
-            console.log(`Successfully wrote item ${i + 1}: ${blogData}`);
-        } catch (error) {
-            console.error(`Error writing item ${i + 1}: ${error}`)
-        }
+        userBlogs.push(blogData);
     };
+    return userBlogs;
 };
 
 const generateUserData = () => {
-    //modify function to generate 500 users and seed to database
-    const prefix = lorem.generateWords(1);
-    const suffix = lorem.generateWords(1);
-    const loremPassword = lorem.generateWords(1).concat(Math.floor(Math.random() * 900) + 100);
-    const data = {
-        name: generateUniqueName(),
-        username: generateUniqueUsername(),
-        email: `${prefix}@${suffix}.com`,
-        password: loremPassword
+    let maxUsers = 100;
+    let count = 0;
+    let userData = []
+
+    while (maxUsers > 0) {
+        const prefix = lorem.generateWords(1);
+        const suffix = lorem.generateWords(1);
+        const loremPassword = lorem.generateWords(1).concat(Math.floor(Math.random() * 900) + 100);
+        
+        const data = {
+            id: generateUniqueId(),
+            name: generateUniqueName(),
+            username: generateUniqueUsername(),
+            email: `${prefix}@${suffix}.com`,
+            password: loremPassword
+        };
+        data['blogs'] = generateBlogData();
+
+        if (count < 25) {
+            userData.push(data);
+        }
+        else if (count === 25) {
+            batchWriteUserData(userData);
+        }
+        count++;
+        maxUsers--;
+    }
+};
+
+const batchWriteUserData = (data) => {
+    const input = {
+        RequestItems: {
+            "users": data.map((item) => ({
+                PutRequest: {
+                    Item: {
+                        id: { S: item.id },
+                        name: { S: item.name },
+                        username: { S: item.username },
+                        email: { S: item.email },
+                        password: { S: item.password },
+                        blogs: {
+                            L: item.blogs.map((blog) => ({
+                                M: {
+                                    id: { S: blog.id.toString() },
+                                    title: { S: blog.title },
+                                    content: { S: blog.content },
+                                },
+                            })),
+                        },
+                    },
+                },
+            })),
+        },
     };
-    return data;
-};
+    dynamoDB.batchWrite(input, (err, data) => {
+        if (err) console.error('Error:', err);
+        else return 'Sucess';
+    });
+}
 
-// todo: identify a way to assign two random blogs to each user
+let data = generateUserData();
+console.log(`data: ${JSON.stringify(data)}`);
 
-
-
-
-module.exports = {
-    // generateUserData,
-    generateBlogData,
-};
+//comment out when running script in terminal
+// module.exports = {
+//     generateUserData,
+// };
