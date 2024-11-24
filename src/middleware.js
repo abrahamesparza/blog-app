75933817b231a8516ec092145740c12f1eb0e7bf
Abraham Esparza <abrahamesparza.dev@gmail.com>
2024-11-23 22:14:52 -0800

    Add middleware for routes

---
+++ src/middleware.js
@@ -0,0 +1,17 @@
+import { NextResponse } from "next/server";
+
+export function middleware(request) {
+    console.log('in middleware function');
+    
+    const sessionId = request.cookies.get('sessionId')?.value;
+
+    if (!sessionId) {
+        return NextResponse.redirect(new URL('/', request.url));
+    };
+
+    return NextResponse.next();
+};
+
+export const config = {
+    matcher: ['/landing', '/write', '/explore', '/profile'],
+};
