a2613d515fcffea1309fd1f75b77b6529a08e6be
Abraham Esparza <abrahamesparza.dev@gmail.com>
2024-11-24 21:05:54 -0800

    Move menu to navigation component, working on passing slug to page

--- src/app/components/navigation.js
+++ src/app/components/navigation.js
@@ -1,16 +1,35 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+
 import styles from '../landing/landingPage.module.css';
 import Logout from '../logout/page';
+import getUsername from '../helpers/getSessionUsername';
+
 
 export default function Navigation() {
+    const router = useRouter();
+   
+    const handleRouting = async (e) => {
+        let page = e.target.innerText.toLowerCase();
+        if (page === 'profile') {
+            const slug = await getUsername();
+            console.log('slug', slug);
+            router.push(`${page}/${slug}`)
+        }
+        // else other path that doesn't have a slug
+        router.push(`/${page}`);
+    }
+    
     return (
         <div className={styles.navigation}>
             {/* improvement idea:
                 create a modal to display below items
                 stacked one above the other
             */}
-            <p className={styles.navItem}>Write</p>
-            <p className={styles.navItem}>Explore</p>
-            <p className={styles.navItem}>Profile</p>
+            <p onClick={handleRouting} className={styles.navItem}>Write</p>
+            <p onClick={handleRouting} className={styles.navItem}>Explore</p>
+            <p onClick={handleRouting} className={styles.navItem}>Profile</p>
             <Logout reroute={'/'}/>
         </div>
     )
