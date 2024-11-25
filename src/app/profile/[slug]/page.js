1bf0a00b21b5be0b0db7d48798afe25d1b4cb06f
Abraham Esparza <abrahamesparza.dev@gmail.com>
2024-11-25 14:37:51 -0800

    Update profile to display data, add css to profile, add notes

--- src/app/profile/[slug]/page.js
+++ src/app/profile/[slug]/page.js
@@ -1,26 +1,41 @@
 'use client';
-
 import { useState, useEffect } from "react";
+import { usePathname, useRouter } from "next/navigation";
+
 import styles from '../profile.module.css';
 import Navigation from "@/app/components/navigation";
 
-export default function Profile({ username, title, content, }) {
+export default function Profile() {
+  const pathName = usePathname();
+  const username = pathName.split('/')[2];
+
+  // write logic to get blog data for username
+  // displays in blogList container
     return (
-      <div>
-        {/* navigation div */}
-        {/* container with two children in grid column */}
-          {/* 1st child displays data in a row 
-          - name
-          - about
-          - list of blogs
-          */}
-          {/* 2nd child displays data in a row
-          - profile poto
-          - # of followers
-          - name
-          - edit profile link
-          - list of following (paginate to 5) */}
+      <div className={styles.profile}>
           <Navigation />
+          <div className={styles.container}>
+
+            <div className={styles.profileChildOne}>
+              <h3 className={styles.username}>@{username}</h3>
+              <div className={styles.follows}>
+                <p>700 Followers</p>
+                <p>300 Following</p>
+              </div>
+              <div className={styles.aboutContainer}>
+                <p className={styles.aboutText}>About section for user.</p>
+              </div>
+              <div className={styles.blogList}>
+                <p>display a list of blogs here</p>
+              </div>
+            </div>
+
+            <div className={styles.profileChildTwo}>
+              {/* replace with settings icon */}
+              <p>Edit</p> 
+            </div>
+
+          </div>
       </div>
     );
   }
