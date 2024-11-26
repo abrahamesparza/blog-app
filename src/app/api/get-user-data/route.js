9c2fe9d07899592778eeb08951416d28873f7b96
Abraham Esparza <abrahamesparza.dev@gmail.com>
2024-11-25 17:02:05 -0800

    Add api to get user data

---
+++ src/app/api/get-user-data/route.js
@@ -0,0 +1,30 @@
+import { NextResponse } from "next/server";
+import dynamoDB from "../lib/dynamodb";
+
+export async function GET(request) {
+    const { searchParams } = new URL(request.url);
+    const username = searchParams.get('username');
+    
+    if (!username) {
+        return NextResponse.json({ message: 'No username in query' });
+    }
+
+    try {
+        const params = {
+            TableName: 'users',
+            IndexName: 'username-index',
+            KeyConditionExpression: 'username = :username',
+            ExpressionAttributeValues: {
+                ':username': username,
+            },
+            ProjectionExpression: 'id, blogs'
+        };
+        const data = await dynamoDB.query(params).promise();
+        let blogs = data.Items[0].blogs;
+        console.log(blogs)
+        return NextResponse.json(blogs);
+    } catch (error) {
+        console.error(error)
+        return NextResponse.json({ error: 'Failed to fetch data' })
+    }
+}
