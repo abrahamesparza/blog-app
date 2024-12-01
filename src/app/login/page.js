9e5748901cb25817d654136a543f92aba4c33d11
Abraham Esparza <abrahamesparza.dev@gmail.com>
2024-11-30 21:50:37 -0800

    Store username in localStorage

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -60,6 +60,7 @@ export default function LoginForm() {
         const result = await response.json();
         console.log('setting loginResult', result.message)
         setLoginResult(result.message);
+        localStorage.setItem('loggedInUser', result.username);
         setFormData(initialFormData);
         setCheckLogin(true)
     }
