267a5de18931ec82d429e29bc9da1b94d3aaba36
Abraham Esparza <abrahamesparza.dev@gmail.com>
2024-12-01 09:42:20 -0800

    Pass function prop to redirect to blog page

--- src/app/components/blogItem.js
+++ src/app/components/blogItem.js
@@ -1,11 +1,13 @@
 import styles from '../profile/profile.module.css';
 
-const BlogItem = ({ title, content, timestamp }) => {
+const BlogItem = ({ title, content, timestamp, handleBlogRoute }) => {
     // const formattedTimestamp = new Date(timestamp).toLocaleString();
   
     return (
       <div className={styles.listItem}>
-        <h4 className={styles.blogTitle}>{title}</h4>
+        <h4
+        className={styles.blogTitle}
+        onClick={handleBlogRoute}>{title}</h4>
         <p>{content.substr(0, 100)}...</p>
         {/* <p className={styles.cardTimestamp}>{formattedTimestamp}</p> */}
       </div>
