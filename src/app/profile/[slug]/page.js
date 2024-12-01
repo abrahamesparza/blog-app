974c037d5b62b1b58cd91f9e428126931260812e
Abraham Esparza <abrahamesparza.dev@gmail.com>
2024-11-30 21:50:13 -0800

    Add conditional rendering

--- src/app/profile/[slug]/page.js
+++ src/app/profile/[slug]/page.js
@@ -1,6 +1,7 @@
 'use client';
 import { useState, useEffect } from "react";
 import { usePathname } from "next/navigation";
+import { CiSettings } from 'react-icons/ci';
 
 import styles from '../profile.module.css';
 import Navigation from "@/app/components/navigation";
@@ -10,58 +11,73 @@ export default function Profile() {
   const pathName = usePathname();
   const username = pathName.split('/')[2];
   const [blogs, setBlogs] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [loggedInUser, setLoggedInUser] = useState(null);
 
   useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const storedUser = localStorage.getItem('loggedInUser');
+      setLoggedInUser(storedUser);
+    }
     getBlogs();
   }, []);
 
   const getBlogs = async () => {
-    const storedBlogs = localStorage.getItem('blogs');
-    if (storedBlogs) {
-      setBlogs(JSON.parse(storedBlogs));
-      return;
+    const storedBlogs = JSON.parse(localStorage.getItem('blogs')) || {}; // Get all blogs or an empty object
+  
+    if (storedBlogs[username]) {
+      setBlogs(storedBlogs[username]);
+      setLoading(false);
+    } else {
+      try {
+        const response = await fetch(`/api/get-user-data?username=${username}`);
+        const data = await response.json();
+        setBlogs(data.blogs || []);
+        const updatedBlogs = { ...storedBlogs, [username]: data.blogs || [] };
+        localStorage.setItem('blogs', JSON.stringify(updatedBlogs));
+        setLoading(false);
+      } catch (error) {
+        console.error(error);
+        setLoading(false);
+      }
     }
-
-    const response = await fetch(`/api/get-user-data?username=${username}`, {
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-    const data = await response.json();
-    setBlogs(data || []);
-    localStorage.setItem('blogs', JSON.stringify(data));
   };
+  
+  return (
+    <div className={styles.profile}>
+        <Navigation />
+        <div className={styles.container}>
 
-    return (
-      <div className={styles.profile}>
-          <Navigation />
-          <div className={styles.container}>
-
-            <div className={styles.profileChildOne}>
-              <h3 className={styles.username}>@{username}</h3>
-              <div className={styles.follows}>
-                <p>700 Followers</p>
-                <p>300 Following</p>
-              </div>
-              <div className={styles.aboutContainer}>
-                <p className={styles.aboutText}>About section for user.</p>
-              </div>
-              <div className={styles.blogList}>
-                <h3 className={styles.blogHeader}>Blogs</h3>
+          <div className={styles.profileChildOne}>
+            <h3 className={styles.username}>@{username}</h3>
+            <div className={styles.follows}>
+              <p>700 Followers</p>
+              <p>300 Following</p>
+            </div>
+            <div className={styles.aboutContainer}>
+              <p className={styles.aboutText}>About section for user.</p>
+            </div>
+            <div className={styles.blogList}>
+              <h3 className={styles.blogHeader}>Blogs</h3>
+              {loading ? (
+                <p>Loading...</p>
+              ) : blogs.length > 0 ? (
                 <ul className={styles.blogUl}>
                   {blogs.map((item, index) => (
                     <BlogItem key={index} title={item.title} content={item.content} />
                   ))}
                 </ul>
-              </div>
+              ) : (
+                <p>No blogs available</p>
+              )}
             </div>
-
-            <div className={styles.profileChildTwo}>
-              {/* replace with settings icon */}
-              <p>Edit</p> 
-            </div>
-
           </div>
-      </div>
-    );
-  }
+
+          <div className={styles.profileChildTwo}>
+            {loggedInUser === username ? <CiSettings size={25}/> : null}
+          </div>
+
+        </div>
+    </div>
+  );
+}
