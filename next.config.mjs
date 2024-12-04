043198dce92da4fb3bfe5447bbe23dbcc93bc7e7
Abraham Esparza <abrahamesparza.dev@gmail.com>
2024-12-04 00:08:04 -0800

    Configure cors policy for external image calls

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [{
+            protocol: 'https',
+            hostname: 'users-pfp.s3.amazonaws.com',
+            pathname: '/profiles/**',
+        },
+    ],
+    },
+};
 
 export default nextConfig;
