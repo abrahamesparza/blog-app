7ba0ab174480b6a9c5e862ba81063998f93113ff
Abraham Esparza <abrahamesparza.dev@gmail.com>
2024-12-03 23:30:21 -0800

    Create edit page to display edit options, and conditional rendering

--- src/app/profile/[slug]/edit/page.js
+++ src/app/profile/[slug]/edit/page.js
@@ -1,70 +1,91 @@
 'use client';
 import { useState, useEffect } from 'react';
+import { IoIosArrowRoundBack } from 'react-icons/io';
+import { useRouter } from 'next/navigation';
 
 import Navigation from '@/app/components/navigation';
+import UploadPhoto from '@/app/components/uploadPhoto';
+import styles from '../../profile.module.css';
 
 export default function Edit() {
-    const [file, setFile] = useState(null);
-    const [uploadStatus, setUploadStatus] = useState('');
-    const [username, setUsername] = useState(null)
+    const [option, setOption] = useState(null);
+    const [isEditing, setIsEditing] = useState(false);
+    const [username, setUsername] = useState(null);
+    const router = useRouter();
 
     useEffect(() => {
-        const user = localStorage.getItem('loggedInUser');
+        let user = localStorage.getItem('loggedInUser');
         setUsername(user);
-    }, [])
+    })
 
-    const handleFileChange = (e) => {
-        let targetFile = e.target.files[0];
-        setFile(targetFile);
+    const editList = [
+        'Upload a photo',
+        'Add a note about yourself',
+        'Update username',
+        'Update password',
+    ]
+
+    const handleEditOption = (e) => {
+        let text = e.target.innerText;
+        if (text === 'Upload a photo') {
+            setOption('Photo');
+        }
+        else if (text === 'Add a note aout yourself') {
+            setOption('About');
+        }
+        else if (text === 'Update username') {
+            setOption('Username');
+        }
+        else if (text === 'Update password') {
+            setOption('Password');
+        }
+        setIsEditing(true);
     };
 
-    const handleUpload = async () => {
-        if (!file) {
-            setUploadStatus('Select a file first');
-            return;
-        }
-
-        try {
-            const response = await fetch('/api/upload-pfp', {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({
-                    username,
-                    fileName: file.name
-                }),
-            });
-
-            const { uploadUrl } = await response.json();
-            await fetch(uploadUrl, {
-                method: 'PUT',
-                headers: {
-                    'Content-Type': file.type,
-                },
-                body: file,
-            });
-
-            if (response.error) {
-                setUploadStatus('Upload failed');
-                console.log('Upload failed');
-            }
-            else {
-                setUploadStatus('File uploaded successfully');
-            }
-        }
-        catch (error) {
-            console.error('Error uploading file:', error)
+    const renderComponent = () => {
+        switch(option) {
+            case 'Photo':
+                return <UploadPhoto />
+            default:
+                return;
         }
     }
 
+    const updateEditingStatus = () => {
+        if (isEditing) {
+            setIsEditing(false);
+            setOption(null);
+        }
+        else {
+            router.push(`/profile/${username}`);
+        }
+    };
+
     return (
-        <div>
+        <div className={styles.profile}>
             <Navigation />
-            <div>
-                <h2>Upload Profile Picture</h2>
-                <input type="file" accept="image/*" onChange={handleFileChange} />
-                <button onClick={handleUpload}>Upload</button>
-                {uploadStatus && <p>{uploadStatus}</p>}
+            <div className={styles.editContainer}>
+                <IoIosArrowRoundBack className={styles.backButton} onClick={updateEditingStatus} size={28} />
             </div>
+            <div className={styles.editContainer} style={{ display: isEditing ? 'none' : 'block' }}>
+                <div className={styles.selectOptionText}>
+                    <h3>Select an option to edit</h3>
+                </div>
+                <div className={styles.editList}>
+                    <ul>
+                        {editList.map((item, index) => (
+                            <li
+                            key={index}
+                            className={styles.editItems}
+                            onClick={handleEditOption}
+                            >
+                                {item}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+            {renderComponent()}
         </div>
     )
 }
