b42ec52c84d2ec0612c66b24e39e610aec0089b5
Abraham Esparza <abrahamesparza.dev@gmail.com>
2024-12-11 23:03:11 -0800

    Display friend requests

--- src/app/components/friendRequests.js
+++ src/app/components/friendRequests.js
@@ -1,12 +1,38 @@
 'use client';
 import { useState, useEffect } from "react";
-import { useRouter } from 'next/navigation';
+import { useRouter, usePathname } from 'next/navigation';
 
-export default function FriendRequests({ requests }) {
-    console.log('friend requests', requests);
+export default function FriendRequests() {
+    const pathName = usePathname();
+    const username = pathName.split('/')[2];
+    const [friendRequests, setFriendRequests] = useState([]);
+
+    useEffect(() => {
+        getFriendRequests();
+    }, [])
+
+    const getFriendRequests = async () => {
+        try {
+            const response = await fetch(`/api/get-friend-requests?username=${username}`);
+            const data = await response.json();
+            if (data.error) {
+                console.error('Error fetching friend requets');
+            }
+            else {
+                setFriendRequests(data.friendRequests);
+            }
+        }
+        catch (error) {
+            console.error('Error fetching friend requests.');
+        }
+    };
+
     return (
         <div>
-            <p>Friend requests page</p>
+            <p>Friend Requests</p>
+            {friendRequests.map((item, index) => (
+                <p>{item}</p>
+            ))}
         </div>
     )
 };
