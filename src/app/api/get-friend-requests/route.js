a675be97ca2791d6b2857cf20e7b82e2e970f261
Abraham Esparza <abrahamesparza.dev@gmail.com>
2024-12-11 17:06:11 -0800

    Write api to get users friend requests

---
+++ src/app/api/get-friend-requests/route.js
@@ -0,0 +1,28 @@
+import { NextResponse } from "next/server";
+
+import dynamoDB from "../lib/dynamodb";
+
+export async function GET(request) {
+    const { searchParams } = new URL(request.url);
+    const username = searchParams.get('username');
+
+    try {
+        const params = {
+            TableName: 'users',
+            IndexName: 'profile-data-index',
+            KeyConditionExpression: 'username = :username',
+            ExpressionAttributeValues: {
+                ':username': username,
+            },
+            ProjectionExpression: 'id, friendRequests',
+        };
+        
+        const response = await dynamoDB.query(params).promise();
+        const data = response.Items[0];
+        return NextResponse.json({ message: 'Success', friendRequests: data.friendRequests });
+    }
+    catch (error) {
+        console.error('Error retrieving friend reqeuests', error);
+        return NextResponse.json({ Error: 'Error fetching friend requests' });
+    }
+}
