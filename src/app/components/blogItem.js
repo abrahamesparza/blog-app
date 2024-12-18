9ab89e84c9cbfbbddfe6f36f07bc3deda0258580
Abraham Esparza <abrahamesparza.dev@gmail.com>
2024-12-18 11:09:13 -0800

    Update blogItem to include timestamp

--- src/app/components/blogItem.js
+++ src/app/components/blogItem.js
@@ -1,15 +1,24 @@
 import styles from '../profile/profile.module.css';
 
 const BlogItem = ({ title, content, timestamp, handleBlogRoute }) => {
-    // const formattedTimestamp = new Date(timestamp).toLocaleString();
+    const formattedTimestamp = new Date(timestamp).toLocaleString('en-US', {
+      year: 'numeric',
+      month: '2-digit',
+      day: '2-digit',
+      hour: '2-digit',
+      minute: '2-digit',
+      hour12: false
+    });
   
     return (
       <div className={styles.listItem}>
-        <h4
-        className={styles.blogTitle}
-        onClick={handleBlogRoute}>{title}</h4>
+        <div className={styles.titleTime}>
+          <h3
+          className={styles.blogTitle}
+          onClick={handleBlogRoute}>{title}</h3>
+          <p className={styles.cardTimestamp}>{formattedTimestamp}</p>
+        </div>
         <p>{content.substr(0, 100)}...</p>
-        {/* <p className={styles.cardTimestamp}>{formattedTimestamp}</p> */}
       </div>
     );
   };
