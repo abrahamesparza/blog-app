d3d47ede82ba7b7586682729c9e295fddf08da16
Abraham Esparza <abrahamesparza.dev@gmail.com>
2024-12-24 13:57:26 -0800

    Remove friend requests from useeffect

--- src/app/components/friendRequests.js
+++ src/app/components/friendRequests.js
@@ -21,7 +21,7 @@ export default function FriendRequests() {
         setLoggedInUserId(userId);
 
         getFriendRequests();
-    }, [friendRequests]);
+    }, []);
 
     const getFriendRequests = async () => {
         try {
