e4e8a3bfcaa42bf2979af46571ee0c2641baae4f
Abraham Esparza <abrahamesparza.dev@gmail.com>
2024-12-27 12:25:00 -0800

    Add handle navigation for username profile routing

--- src/app/components/blogItem.js
+++ src/app/components/blogItem.js
@@ -1,5 +1,6 @@
 import styles from '../profile/profile.module.css';
 import { useRouter } from 'next/navigation';
+import { SiNicehash } from "react-icons/si";
 
 const BlogItem = ({ author, title, content, timestamp, handleBlogRoute }) => {
   const router = useRouter();
@@ -13,20 +14,28 @@ const BlogItem = ({ author, title, content, timestamp, handleBlogRoute }) => {
       hour12: false
     });
 
-    const handleNavigation = (username) => {
+    const handleReaction = async (e) => {
+      e.stopPropagation();
+      //write api to store likes and retrieve
+      alert('reaction alert');
+    };
+
+    const handleNavigation = (e, username) => {
+      e.stopPropagation();
       router.push(`/profile/${username}`);
-    }
+    };
   
     return (
       <div className={styles.listItem} onClick={() => handleBlogRoute(title)}>
         <div className={styles.titleTime}>
           <div className={styles.authorTitle}>
             <h3 className={styles.blogTitle}>{title}</h3>
-            <p onClick={() => handleNavigation(author)} className={styles.author}>@{author}</p>
+            <p onClick={(e) => handleNavigation(e, author)} className={styles.author}>@{author}</p>
           </div>
           <p className={styles.cardTimestamp}>{formattedTimestamp}</p>
         </div>
         <p>{content.substr(0, 100)}...</p>
+        <SiNicehash className={styles.reaction} size={12} onClick={handleReaction}/>
       </div>
     );
   };
