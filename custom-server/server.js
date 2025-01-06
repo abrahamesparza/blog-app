f7acdcedaa1ba00de12f5fc7f4a56f373e7da6a8
Abraham Esparza <abrahamesparza.dev@gmail.com>
2025-01-06 13:30:40 -0800

    Update var name and remove comment

--- custom-server/server.js
+++ custom-server/server.js
@@ -10,8 +10,7 @@ const handle = app.getRequestHandler();
 if (cluster.isMaster) {
     console.log(`Master ${process.pid} is running`);
 
-    // Fork workers
-    for (let i = 0; i < numCPUs; i++) {
+    for (let i = 0; i < cpuCount; i++) {
         cluster.fork();
     }
 
